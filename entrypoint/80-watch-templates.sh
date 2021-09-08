933517da376f6893c2926b4e273cc6325d57320a
Jay T <jaytula@gmail.com>
2021-09-08 16:33:41 -0700

    Target the correct template directory

--- entrypoint/80-watch-templates.sh
+++ entrypoint/80-watch-templates.sh
@@ -4,8 +4,10 @@
 # - Run 20-envsubst-on-templates.sh
 # - Reload nginx `with nginx -s reload`
 
-function watch {
-  while inotifywait --exclude .swp -e modify -r templates; do
+watch() {
+ local template_dir="${NGINX_ENVSUBST_TEMPLATE_DIR:-/etc/nginx/templates}"  
+
+  while inotifywait --exclude .swp -e modify -r $template_dir; do
     /docker-entrypoint.d/20-envsubst-on-templates.sh
     nginx -s reload
   done
